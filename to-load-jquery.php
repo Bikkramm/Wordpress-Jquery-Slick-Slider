eb84621427cc598b40ca967c49f587180b01403d
Bikkramm <144205863+Bikkramm@users.noreply.github.com>
2025-12-24 10:35:01 +0530

    Update to-load-jquery.php

--- to-load-jquery.php
+++ to-load-jquery.php
@@ -1,3 +1,9 @@
+// note the added jquery should not be in $ format it should be with jQuery format
+for example:- my.js
+jQuery(document).ready(function() {
+    alert('working');
+});
+
 <?php
 function myscript() {
     wp_enqueue_script(
